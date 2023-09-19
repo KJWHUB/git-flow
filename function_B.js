5e65ef0f9f1f193cef26719366974d678fbaedbd
kjw1025_mac_studio <126055193+KJWHUB@users.noreply.github.com>
2023-09-19 15:11:43 +0900

    Feature: B2

--- function_B.js
+++ function_B.js
@@ -1,4 +1,7 @@
 // 리스트 가공 기능
-const processArray = () => {
-  return [];
+const processArray = (obj) => {
+  if (typeof obj !== "object") {
+    return console.error("잘못된 파라미터 타입 입니다.");
+  }
+  return Object.values(obj).map((el) => el.stock === 0);
 };
