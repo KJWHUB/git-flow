b70685c7ac4140b00412463eb44df113456c0699
KJWHUB <126055193+KJWHUB@users.noreply.github.com>
2024-08-18 23:04:43 +0900

    feat: product module create

---
+++ product/index.js
@@ -0,0 +1,46 @@
+function Product({ elementId }) {
+  const list = [];
+
+  const createProduct = (name, price) => {
+    return {
+      name,
+      price,
+    };
+  };
+
+  const add = (product) => {
+    list.push(product);
+  };
+  const remove = (product) => {
+    list.splice(list.indexOf(product), 1);
+  };
+
+  const setup = () => {
+    console.group("Product setup");
+    try {
+      const element = document.getElementById(elementId);
+      console.log("Element Check:", element);
+
+      if (!element) {
+        throw new Error("Element not found");
+      }
+
+      const products = Array.from({ length: 5 }, (_, i) => createProduct(`Product ${i + 1}`, 100 * (i + 1)));
+
+      products.forEach((product) => {
+        add(product);
+      });
+
+      console.log("Product List:", list);
+    } catch (error) {
+      console.error("Product Setup Fail:", error);
+    }
+    console.groupEnd();
+  };
+
+  return {
+    setup,
+  };
+}
+
+return Product;
